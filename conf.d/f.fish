90be2afce3d9e36ecd094c3ef3fb9d02041af907
Gokul <gokulps15@gmail.com>
2020-06-26 20:58:20 +0530

    Move uninstall logic
    See https://github.com/jorgebucaran/fisher/issues/543

--- conf.d/f.fish
+++ conf.d/f.fish
@@ -31,3 +31,16 @@ end
 
 # Setup completions once first
 __f_complete
+
+function f_uninstall --on-event f_uninstall
+    functions -e __f_on_event_postexec
+    functions -e $F_CMD
+
+    if test -n "$F_DATA"
+        echo "To purge f's data, remove: $F_DATA" > /dev/stderr
+    end
+
+    set -e F_CMD
+    set -e F_DATA
+    set -e F_EXCLUDE
+end
