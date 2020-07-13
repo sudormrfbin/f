2de1e72678c538c7590619806d6db41930032a42
Gokul Soumya <gokulps15@gmail.com>
2020-07-13 21:26:02 +0530

    Suppress bind errors when keys are not initially set
    Closes #3

--- conf.d/f_key_bindings.fish
+++ conf.d/f_key_bindings.fish
@@ -8,18 +8,18 @@ function __f_insert_from_picker
     commandline -f repaint
 end
 
-if not bind \ek
+if not bind \ek 2>/dev/null
     bind \ek __f_insert_from_picker
 end
 
-if not bind -M insert \ek
+if not bind -M insert \ek 2>/dev/null
     bind -M insert \ek __f_insert_from_picker
 end
 
 function f_key_bindings_uninstall --on-event f_key_bindings_uninstall
-    bind \ek | string match -e __f_insert_from_picker
+    bind \ek 2>/dev/null | string match -e __f_insert_from_picker
     and bind -e \ek
 
-    bind -M insert \ek | string match -e __f_insert_from_picker
+    bind -M insert \ek 2>/dev/null | string match -e __f_insert_from_picker
     and bind -M insert -e \ek
 end
